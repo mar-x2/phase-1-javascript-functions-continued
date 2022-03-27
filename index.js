3539ab9c7f9415081471e3c36595cb3f4dc3b88e
Maria Marquez <406rem33@gmail.com>
2022-03-26 22:37:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity){
+    if(activity){
+        return ( `This Saturday, I want to ${activity}!`);
+    }
+    return ('This Saturday, I want to roller-skate!')
+};
+
+function mondayWork(activity){
+    if(activity){
+        return("This Monday, I will work from home.")
+    };
+    return ("This Monday, I will go to the office.");
+};
+
+function wrapAdjective(msg)
+{
+    const august = function(name, lang = "You are") 
+    {
+    return `${lang} ${msg}${name}${msg}!`
+    }
+
+    return august;
+}
